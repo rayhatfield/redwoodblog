71f64e71a4d8faf40da2907fe5cefb8b7738e97c
Ray Hatfield <ray.hatfield@gmail.com>
2023-06-04 15:49:41 -0500

    add auth

--- web/src/layouts/BlogLayout/BlogLayout.js
+++ web/src/layouts/BlogLayout/BlogLayout.js
@@ -1,12 +1,29 @@
 import { Link, routes } from '@redwoodjs/router'
 
+import { useAuth } from 'src/auth'
+
 const BlogLayout = ({ children }) => {
+  const { isAuthenticated, currentUser, logOut } = useAuth()
+  console.log(JSON.stringify(currentUser, null, 3))
+
   return (
     <>
       <header>
-        <h1>
-          <Link to={routes.home()}>Redwood Blog</Link>
-        </h1>
+        <div className="flex-between">
+          <h1>
+            <Link to={routes.home()}>Redwood Blog</Link>
+          </h1>
+          {isAuthenticated ? (
+            <div>
+              <span>Logged in as {currentUser.email}</span>{' '}
+              <button type="button" onClick={logOut}>
+                Log Out
+              </button>
+            </div>
+          ) : (
+            <Link to={routes.login()}>Log In</Link>
+          )}
+        </div>
         <nav>
           <ul>
             <li>
